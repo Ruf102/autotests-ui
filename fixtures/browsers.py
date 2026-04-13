d984b21e531dc11f264daa0e32a755e5fabdeebe
Ruf102 <prodigy97@icloud.com>
2026-04-13 13:28:26 +0500

    Плагины Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -33,11 +33,9 @@ def initialize_browser_state(playwright: Playwright) -> None:
     browser.close()
 
 
-
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
     yield context.new_page()
     browser.close()
-
