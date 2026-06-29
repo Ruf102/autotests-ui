e692a797647bc2237db4d004fd0b3b12bda59b45
Ruf102 <prodigy97@icloud.com>
2026-06-29 09:52:33 +0500

    Логирование взаимодействий со страницей

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,6 +1,10 @@
 import allure
 from playwright.sync_api import Page, expect, Locator
 
+from tools.logger import get_logger
+
+logger = get_logger("BASE_ELEMENT")
+
 
 class BaseElement:
     def __init__(self, page: Page, locator: str, name: str):
@@ -13,21 +17,33 @@ class BaseElement:
         return "self element"
 
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
+        locator = self.locator.format(**kwargs)
+        step = f'Getting locator with "data-testid={locator}" at index "{nth}"'
+
         with allure.step(f'Getting locator with "data-test-id-{self.locator}" at index "{nth}"'):
-            locator = self.locator.format(**kwargs)
+            logger.info(step)
             return self.page.get_by_test_id(locator).nth(nth)
 
     def click(self, nth: int = 0, **kwargs):
+        step = f'Clicking {self.type_of} "{self.name}"'
+
         with allure.step(f'Clicking {self.type_of} "{self.name}"'):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             locator.click()
 
     def check_visible(self, nth: int = 0, **kwargs):
+        step = f'Checking that {self.type_of} "{self.name}" is visible'
+
         with allure.step(f'Checking that {self.type_of} "{self.name}" is visible'):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_be_visible()
 
     def check_have_text(self, text: str, nth: int = 0, **kwargs):
+        step = f'Checking that {self.type_of} "{self.name}" has text "{text}"'
+
         with allure.step(f'Checking that {self.type_of} "{self.name}" has text "{text}"'):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_have_text(text)
